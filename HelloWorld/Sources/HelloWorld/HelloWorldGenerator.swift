ea822f3eee53c4e3aef79058cec84242f6406c8e
Florian Rieger <florian.rieger@git.appcron.com>
2020-07-15 13:30:06 +0200

    Added empty public init() to HelloWorldGenerator in order to initilize it from outside of the package.

--- HelloWorld/Sources/HelloWorld/HelloWorldGenerator.swift
+++ HelloWorld/Sources/HelloWorld/HelloWorldGenerator.swift
@@ -1,5 +1,9 @@
 public class HelloWorldGenerator {
 
+    public init() {
+        // Allow class to be initialized from outside the package
+    }
+
     public func createHello(withName name: String) -> String? {
         guard name.count > 0 else {
             return nil
