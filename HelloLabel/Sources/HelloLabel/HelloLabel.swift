7b010fa4a6809926dfc5f6b92a8dac6cb46b1539
Florian Rieger <florian.rieger@git.appcron.com>
2020-07-15 13:35:46 +0200

    Implemented functionality in HelloLabel.

--- HelloLabel/Sources/HelloLabel/HelloLabel.swift
+++ HelloLabel/Sources/HelloLabel/HelloLabel.swift
@@ -1,5 +1,12 @@
 import UIKit
+import HelloWorld
 
 public class HelloLabel: UILabel {
+
+    private let generator = HelloWorldGenerator()
+
+    public func setGreetingText(withName name: String) {
+        text = generator.createHello(withName: name)
+    }
 
 }
